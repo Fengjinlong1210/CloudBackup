d90b21cc0a1b66ad9ef520fe000ff12e04179f3e
Fengjinlong1210 <239383772@qq.com>
2023-08-28 12:44:01 +0800

    任务单元

---
+++ server/task.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "util.hpp"
+
+namespace Cloud
+{
+    class Task
+    {
+    public:
+        Task(){}    //提供一个默认构造函数, 供线程池使用
+        Task(FileUtil util, const std::string &pack_path)
+            : _util(util), _pack_path(pack_path)
+        {
+        }
+
+        bool operator()()
+        {
+            std::cout << "compress" << std::endl;
+            _util.Compress(_pack_path);
+            
+            _util.Remove();
+        }
+
+    private:
+        FileUtil _util;
+        std::string _pack_path;
+    };
+}
